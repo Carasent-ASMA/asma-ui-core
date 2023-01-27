479fa3f5692dd134d0c653e327fe7949e37d57ef
Andrei Grini <Andrei.Grini@avanssoma.no>
2023-01-27 07:45:13 +0000

    Merged PR 2469: refator antrD Modal confirm
    refator antrD Modal confirm

--- src/rd-components/modals/modal-confirm/AtrdModalConfirm.tsx
+++ src/rd-components/modals/modal-confirm/AtrdModalConfirm.tsx
@@ -5,38 +5,33 @@ import type { ModalProps } from 'antd/es'
 
 interface IAtrdModalConfirmProps extends ModalProps {
     content: string
+    cancelText:string
+    okText:string
+    title:string
 }
 
-export const AtrdModalConfirm: React.FC<IAtrdModalConfirmProps> = ({
-    open: visible,
-    onCancel,
-    onOk,
-    content,
-    title,
-    cancelText,
-    okText,
-}) => {
+export const AtrdModalConfirm: React.FC<IAtrdModalConfirmProps> = ( props ) => {
     return (
         <Modal
-            centered
-            className='antrd-modal-confirm'
-            closable={false}
-            onCancel={onCancel}
-            open={visible}
+        {...props}
+            
+            className={`antrd-modal-confirm ${props.className}`}
+            
             footer={
                 <>
-                    <button className='antd-confirm-modal-body_cancel-button' onClick={onCancel}>
-                        {cancelText}
+                    <button className='antd-confirm-modal-body_cancel-button' onClick={props.onCancel}>
+                        {props.cancelText}
                     </button>
-                    <button className='antd-confirm-modal-body_confirm-button' onClick={onOk}>
-                        {okText}
+                    <button className='antd-confirm-modal-body_confirm-button' onClick={props.onOk}>
+                        {props.okText}
                     </button>
                 </>
             }
             width={343}
         >
-            <div className='pb-4 font-semibold'>{title}</div>
-            <div>{content}</div>
+           
+            <div className='pb-4 font-semibold'>{props.title}</div>
+            <div>{props.content}</div>
         </Modal>
     )
 }
