bcc9515f3aed2f8da0e48a876d569b4bad335f63
Sebastian Darie <Sebastian.Darie@carasent.com>
2023-05-24 10:17:30 +0000

    Merged PR 3107: added new classes, changed styles to match design, sx props broken
    added new classes, changed styles to match design, sx props broken
    
    Related work items: #3583

--- src/components/DataGrid/StyledDataGrid.tsx
+++ src/components/DataGrid/StyledDataGrid.tsx
@@ -5,21 +5,28 @@ export const StyledDataGrid = styled((props: DataGridProps) => (
     <DataGrid
         {...props}
         classes={{
-            root: 'bg-white border-0 text-[#0b0c0f] text-sm',
-            actionsCell: 'text-black text-2xl',
-            cell: 'text-[#0b0c0f] text-sm border-b border-[#b5bec9] focus:outline-none focus-within:outline-none',
-            columnHeader: 'border-r-0 text-[#7a899e] text-xs h-[30px] focus:outline-none focus-within:outline-none',
-            columnHeaders: 'bg-gray-100 border-b border-[#b5bec9] min-h-[40px]',
+            root: 'bg-white border-0 border-t border-[#bdc4cf] rounded-none',
+            cell: 'border-b border-[#bdc4cf] min-h-[45px] max-h-[45px] focus:outline-none focus-within:outline-none',
+            cellContent: 'text-[#0b0c0f] text-base',
+            columnHeader: 'border-r-0 h-[30px] focus:outline-none focus-within:outline-none',
+            columnHeaderTitle: 'text-[#626e7e] text-xs font-semibold uppercase',
+            columnHeaders: 'bg-[#f9fafb] border-b border-[#bdc4cf] min-h-[30px] rounded-none',
+            footerContainer: 'border-[#bdc4cf]',
             iconSeparator: 'hidden',
-            row: 'hover:bg-primary-50',
+            row: 'min-h-[45px] max-h-[45px] hover:bg-primary-50 hover:cursor-pointer',
             virtualScrollerContent: 'border-b-0',
         }}
-        sx={{
-            ...props.sx,
-            '.MuiDataGrid-row.Mui-selected': {
-                backgroundColor: 'white',
+        sx={(theme) => ({
+            //TODO: fix sx props, right now you can't use sx props with the DataGrid
+            // ...props.sx,
+            '.MuiDataGrid-actionsCell .MuiSvgIcon-root': {
+                color: 'black',
+                fontSize: '1.5rem',
             },
-        }}
+            '.MuiDataGrid-row.Mui-selected': {
+                backgroundColor: theme.palette.primary[50],
+            },
+        })}
         slotProps={{
             ...props.slotProps,
             baseCheckbox: {
