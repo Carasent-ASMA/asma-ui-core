4a6f57737108d5b73bf0051632c7af7a3f48a4c0
Bragari Andrei <bragari.andrei@carasent.com>
2023-10-02 09:53:28 +0000

    Merged in feature/ASMA-2342-create-a-new-Inputs-in-core-ui (pull request #76)
    feat: Added new custom input
    
    * fix: Fixed icon position & added className
    
    * Merge branch 'feature/ASMA-2342-create-a-new-Inputs-in-core-ui' of https://bitbucket.org/carasent/asma-core-ui into feature/ASMA-2342-create-a-new-Inputs-in-core-ui
    
    * Merge branch 'master' of bitbucket.org:carasent/asma-core-ui into feature/ASMA-2342-create-a-new-Inputs-in-core-ui
    
    * fix: Styled Input bugs
    
    * deleted pb/ change outline/ changed input height
    
    * Merge branch 'master' of bitbucket.org:carasent/asma-core-ui into feature/ASMA-2342-create-a-new-Inputs-in-core-ui
    
    * fix: Input style fix
    
    * Merge branch 'master' of bitbucket.org:carasent/asma-core-ui into feature/ASMA-2342-create-a-new-Inputs-in-core-ui
    
    * feat: Styled Input
    
    * styled input
    
    
    Approved-by: Andrei Grini

---
+++ src/components/inputs/input/StyledInput.stories.tsx
@@ -0,0 +1,137 @@
+import React, { useState, type ChangeEvent } from 'react'
+import type { Meta } from '@storybook/react'
+
+import { Icon } from '@iconify/react'
+import { StyledInput } from './StyledInput'
+import { StyledButton } from 'src/components/inputs/button/StyledButton'
+
+const meta = {
+    title: 'Inputs/Styled Input',
+    component: StyledInput,
+    tags: ['autodocs'],
+    argTypes: {},
+    args: {
+        label: 'Label',
+        placeholder: 'Placeholder text',
+        startIcon: <Icon icon='mdi:account-outline' width={24} height={24} />,
+        endIcon: <Icon icon='mdi:magnify' width={24} height={24} />,
+        descriptionMessage: 'Description message',
+        required: false,
+        value: '',
+        type: 'text',
+        id: '',
+        dataTest: '',
+        size: 'large',
+        className: '',
+    },
+} satisfies Meta<typeof StyledInput>
+
+export default meta
+export const Inputs = () => {
+    const [inputText, setInputText] = useState('some test text')
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value)
+    }
+    const showDescription = inputText.length < 5
+    return (
+        <div className='flex flex-col w-full gap-12'>
+            <StyledInput {...meta.args} onChange={handleInputChange} value={inputText} label='Label text' />
+            <StyledInput
+                {...meta.args}
+                onChange={handleInputChange}
+                value={inputText}
+                error={inputText.length < 5}
+                label='Error'
+                descriptionMessage={showDescription ? 'Error message' : ''}
+            />
+            <StyledInput
+                onChange={handleInputChange}
+                value={inputText}
+                disabled
+                label='Disabled'
+                descriptionMessage='Disabled message'
+            />
+            <StyledInput
+                {...meta.args}
+                onChange={handleInputChange}
+                value={inputText}
+                notEditable
+                label='Not editable'
+                descriptionMessage='Not editable message'
+            />
+            <StyledInput
+                {...meta.args}
+                onChange={handleInputChange}
+                value={inputText}
+                readOnly
+                label='Read only'
+                descriptionMessage='Read only message'
+            />
+            <StyledInput
+                onChange={handleInputChange}
+                value={inputText}
+                label='small input'
+                dataTest='123'
+                size='small'
+            />
+            <div className='flex gap-2 items-center justify-center'>
+                <StyledInput
+                    onChange={handleInputChange}
+                    placeholder='StyledInput'
+                    value={inputText}
+                    dataTest='StyledInput'
+                />
+                <StyledButton>Button Example</StyledButton>
+            </div>
+            <div className='flex gap-2 items-center justify-center bg-green-50 p-5'>
+                <StyledInput
+                    onChange={handleInputChange}
+                    placeholder='StyledInput'
+                    value={inputText}
+                    dataTest='StyledInput'
+                    size='small'
+                />
+                <StyledButton size='small'>Button Example Small</StyledButton>
+            </div>
+            <div className='flex gap-2 w-[500px]  relative bg-yellow-50 p-5'>
+                <StyledInput
+                    onChange={handleInputChange}
+                    placeholder='Small input'
+                    value={inputText}
+                    dataTest='StyledInput'
+                    size='small'
+                    error
+                    // descriptionMessage='Describe me'
+                    label='Small with error'
+                    className='flex flex-auto w-full'
+                />
+                <StyledButton size='small'>Button Example Small</StyledButton>
+            </div>
+            <div className='flex gap-2 w-[500px]  relative bg-green-50'>
+                <StyledInput
+                    onChange={handleInputChange}
+                    placeholder='StyledInput'
+                    value={inputText}
+                    dataTest='StyledInput'
+                    error
+                    descriptionMessage='Describe me'
+                    label='ewfwefwe'
+                    endIcon={<Icon icon='mdi:magnify' width={24} height={24} />}
+                />
+                <StyledButton>Button Example Small</StyledButton>
+            </div>
+            <StyledInput
+                onChange={handleInputChange}
+                placeholder='StyledInput'
+                value={inputText}
+                dataTest='StyledInput'
+                size='small'
+                error
+                descriptionMessage='Describe me'
+                label='ewfwefwe'
+                endIcon={<Icon icon='mdi:magnify' width={24} height={24} />}
+            />
+        </div>
+    )
+}
--- src/components/inputs/input/StyledInput.tsx
+++ src/components/inputs/input/StyledInput.tsx
@@ -1,3 +1,116 @@
-import { Input, type InputProps } from '@mui/material'
+import { type FC } from 'react'
+import styles from './StyledInput.module.scss'
+import clsx from 'clsx'
 
-export const StyledInput = (props: InputProps) => <Input {...props} />
+/**
+ * Developer: bragari.andrei@carasent.com
+ * Custom props:
+ * @param descriptionMessage - message under input (string)
+ * @param startIcon - icon before input (JSX.Element)
+ * @param endIcon - icon after input (JSX.Element)
+ * @param dataTest - data-test tag (string)
+ * @param label - string
+ * @param refLink -  ref to component
+ * @param size -  'large' | 'small'
+ * @param className - for styles (width)
+ */
+type ConditionalProps =
+    | {
+          disabled?: boolean
+          error?: never
+          notEditable?: never
+          readOnly?: never
+      }
+    | {
+          disabled?: never
+          error?: boolean
+          notEditable?: never
+          readOnly?: never
+      }
+    | {
+          disabled?: never
+          error?: never
+          notEditable?: boolean
+          readOnly?: never
+      }
+    | {
+          disabled?: never
+          error?: never
+          notEditable?: never
+          readOnly?: boolean
+      }
+
+type StyledInputProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> & {
+    size?: 'large' | 'small'
+    descriptionMessage?: string
+    startIcon?: JSX.Element
+    endIcon?: JSX.Element
+    dataTest?: string
+    label?: string
+    refLink?: React.Ref<HTMLInputElement>
+} & ConditionalProps
+export const StyledInput: FC<StyledInputProps> = ({
+    descriptionMessage,
+    disabled = false,
+    error = false,
+    label,
+    refLink,
+    startIcon,
+    endIcon,
+    type,
+    readOnly,
+    notEditable,
+    dataTest,
+    size = 'large',
+    className,
+    ...otherProps
+}) => {
+    const inputType = notEditable ? 'text' : type || 'text'
+
+    const sizeClass = size === 'small' ? styles['small'] : ''
+
+    const typeMixin = error
+        ? 'error'
+        : disabled
+        ? 'disabled'
+        : readOnly
+        ? 'readOnly'
+        : notEditable
+        ? 'notEditable'
+        : 'active'
+
+    return (
+        <div className={clsx('relative', className)}>
+            <label htmlFor='custom-input' className={`${styles['label']} ${styles[typeMixin]}`}>
+                {label}
+            </label>
+            <span
+                className={` ${styles['startIcon']} ${styles[typeMixin]} ${typeMixin === 'readOnly' ? 'left-0' : ''}`}
+            >
+                {startIcon}
+            </span>
+            <input
+                {...otherProps}
+                ref={refLink}
+                type={inputType}
+                className={clsx(
+                    styles['input'],
+                    styles[typeMixin],
+                    sizeClass,
+                    typeMixin === 'readOnly' && startIcon && 'pl-8',
+                    typeMixin === 'readOnly' && endIcon && 'pr-8',
+                    startIcon && 'pl-12',
+                    endIcon && 'pr-12',
+                    className,
+                )}
+                disabled={disabled}
+                readOnly={notEditable || readOnly}
+                data-test={dataTest}
+            />
+            <span className={`${styles['endIcon']} ${styles[typeMixin]} ${typeMixin === 'readOnly' ? 'right-0' : ''}`}>
+                {endIcon}
+            </span>
+            <div className={`${styles['description']} ${styles[typeMixin]}`}>{descriptionMessage}</div>
+        </div>
+    )
+}
