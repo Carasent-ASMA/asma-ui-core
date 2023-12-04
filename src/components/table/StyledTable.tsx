1d10882ac2e09aa3003ed0ded6e24937ab4544a0
Maxim Curudimov <maxim.curudimov@carasent.com>
2023-12-04 08:02:58 +0000

    Merged in feat/ASMA-3000-styles-for-focus (pull request #185)
    feat - ASMA-3000: Сhanged styles for focus for styled table
    
    * feat: Сhanged styles for focus for styled table
    
    * Merge branch 'master' of bitbucket.org:carasent/asma-core-ui into feat/ASMA-3000-styles-for-focus
    
    
    Approved-by: Andrei Grini

--- src/components/table/StyledTable.tsx
+++ src/components/table/StyledTable.tsx
@@ -220,9 +220,9 @@ export const StyledTable = <
                                 <tr
                                     data-test={row.id}
                                     id={row.id}
-                                    tabIndex={focusable ? 0: undefined}
+                                    tabIndex={focusable ? -1: undefined}
                                     className={clsx(
-                                        'table-row align-middle border-x-0 border-y border-solid border-delta-300 hover:cursor-pointer hover:bg-primary-25 focus:bg-primary-100',
+                                        'table-row align-middle border-x border-x-transparent border-t border-b-0 last:border-b border-solid border-delta-300 hover:cursor-pointer hover:bg-primary-25 focus:bg-primary-50 focus:border focus:border-primary-500',
                                         (row.getIsExpanded() || row.getIsSelected()) && 'bg-primary-50',
                                         loading && 'opacity-50',
                                         getRowClassName?.(row),
