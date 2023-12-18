70af500d0cf7f16c056a56a5ce7cd13c5e1ce2fd
Andrei Grini <andrei.grini@carasent.com>
2023-12-18 10:43:38 +0000

    Merged in fix/styled-button-ui-stabilized (pull request #199)
    fix: Styled button UI stabilized
    
    * fix: Styled button UI stabilized
    
    * Merge branch 'master' of bitbucket.org:carasent/asma-core-ui into fix/styled-button-ui-stabilized

--- src/components/inputs/button/StyledButton.tsx
+++ src/components/inputs/button/StyledButton.tsx
@@ -1,6 +1,7 @@
 import React, { type ReactNode } from 'react'
 
 import styles from './StyledButton.module.scss'
+import './StyledButton.scss'
 import clsx from 'clsx'
 
 export type StyledButtonType = 'contained' | 'outlined' | 'text' | 'textGray'
@@ -58,26 +59,45 @@ export const StyledButton = ({
     const color = error ? 'error' : 'common'
 
     return (
-        <button
-            {...otherProps}
-            className={clsx(
-                isLarge ? 'px-2' : 'px-1.5',
-                styles['button'],
-                styles[variant],
-                styles[color],
-                styles[size],
-                className,
-            )}
-            ref={refLink}
-            data-test={dataTest}
-        >
-            {startIcon && startIcon}
-            {children && (
-                <div className={`truncate flex items-center justify-center gap-2 ${isLarge ? 'px-2' : 'px-1'}`}>
-                    {children}
-                </div>
-            )}
-            {endIcon && endIcon}
-        </button>
+        <>
+            {/* backup compatibility */}
+            <div style={{ display: 'none' }}>
+                <button
+                    {...otherProps}
+                    className={clsx(
+                        isLarge ? 'px-2' : 'px-1.5',
+                        styles['button'],
+                        styles[variant],
+                        styles[color],
+                        styles[size],
+                        className,
+                    )}
+                    ref={refLink}
+                    data-test={dataTest}
+                >
+                    {startIcon && startIcon}
+                    {children && (
+                        <div className={`truncate flex items-center justify-center gap-2 ${isLarge ? 'px-2' : 'px-1'}`}>
+                            {children}
+                        </div>
+                    )}
+                    {endIcon && endIcon}
+                </button>
+            </div>
+            <button
+                {...otherProps}
+                className={clsx(isLarge ? 'px-2' : 'px-1.5', 'asma-core-ui-button', variant, color, size, className)}
+                ref={refLink}
+                data-test={dataTest}
+            >
+                {startIcon && startIcon}
+                {children && (
+                    <div className={`truncate flex items-center justify-center gap-2 ${isLarge ? 'px-2' : 'px-1'}`}>
+                        {children}
+                    </div>
+                )}
+                {endIcon && endIcon}
+            </button>
+        </>
     )
 }
