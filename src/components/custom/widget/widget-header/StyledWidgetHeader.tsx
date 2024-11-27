fcbb7b093b801c54a944411d7a0d76625e0a32b9
Bularga Alexandru <bularga.alexandru@carasent.com>
2024-11-27 15:48:21 +0000

    Merged in feat/ASMA-add-conditional-classname-props-to-styled-widget-header (pull request #400)
    Feat/ASMA add conditional classname props to styled widget header
    
    * v3.0.124
    
    * feat: Added a couple of conditional props for StyledWidgetHeader component

--- src/components/custom/widget/widget-header/StyledWidgetHeader.tsx
+++ src/components/custom/widget/widget-header/StyledWidgetHeader.tsx
@@ -2,12 +2,18 @@ import React, { type ReactNode } from 'react'
 import { StyledWidgetTitle } from '../widget-title/StyledWidgetTitle'
 
 import style from './StyledWidgetHeader.module.scss'
+import { cn } from 'src/helpers/cn'
 
-export const StyledWidgetHeader: React.FC<{ actions?: ReactNode; title?: string }> = ({ actions, title }) => {
+export const StyledWidgetHeader: React.FC<{
+    actions?: ReactNode
+    title?: string
+    containerClassname?: string
+    actionsClassname?: string
+}> = ({ actions, title, containerClassname, actionsClassname }) => {
     return (
-        <div className={style['styled-widget-header']}>
+        <div className={cn(style['styled-widget-header'], containerClassname)}>
             <StyledWidgetTitle>{title}</StyledWidgetTitle>
-            <div>{actions}</div>
+            <div className={actionsClassname}>{actions}</div>
         </div>
     )
 }
