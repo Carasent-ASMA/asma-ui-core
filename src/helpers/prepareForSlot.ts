7a5791abbe0433f95a51fde2adcbb615e14e7a6b
Sebastian Darie <sebastian.darie@carasent.com>
2024-12-28 20:23:22 +0200

    fix: Added helper to prevent ownerState warning

---
+++ src/helpers/prepareForSlot.ts
@@ -0,0 +1,16 @@
+import { createElement, forwardRef } from 'react'
+
+/**
+ * When inserting a component from a third-party library into a slot, you may encounter this warning: "React does not recognize the ownerState prop on a DOM element." This is because the custom component isn't prepared to receive the ownerState like a built-in library component would be.
+ */
+export function prepareForSlot<ComponentType extends React.ElementType>(Component: ComponentType) {
+    type Props = React.ComponentProps<ComponentType>
+
+    return forwardRef<HTMLElement, Props>(function Slot(props, ref) {
+        const { ownerState, ...other } = props
+        return createElement<Props>(Component, {
+            ...(other as Props),
+            ref,
+        })
+    })
+}
