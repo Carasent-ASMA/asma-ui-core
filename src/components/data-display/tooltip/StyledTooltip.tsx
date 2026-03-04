53a807d7e3c402fa0b08228282c292e54548ed5c
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2026-03-04 23:54:42 +0200

    fix(external): Remove leaveDelay from StyledTooltip component

--- src/components/data-display/tooltip/StyledTooltip.tsx
+++ src/components/data-display/tooltip/StyledTooltip.tsx
@@ -10,7 +10,6 @@ export const StyledTooltip = (props: TooltipProps) => {
             TransitionComponent={Fade}
             TransitionProps={{ timeout: 300 }}
             enterDelay={300}
-            leaveDelay={300}
             placement='top'
             componentsProps={{
                 ...componentsProps,
