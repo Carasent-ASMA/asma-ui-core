5c42053e99806e83a21e7643d876e1acba0fbdbc
igor.efrem <igor.efrem@carasent.com>
2026-04-09 23:32:57 +0200

    chore(deps): Update lint scripts and upgrade @mui/material to 5.18.0
    - Refactor lint/lint:fix scripts for better extension handling
    - Move @emotion/react, @emotion/styled, react-quill to devDependencies
    - Upgrade @mui/material and peerDependencies to 5.18.0
    - Update pnpm-lock.yaml for dependency changes
    
    [ASMA-POLICY] --allow-protected-push was used for this commit flow by igor.efrem.
    Pushing to protected branches may fail or violate team policy if you are not authorized.

--- src/components/utils/accordion/base-ui/StyledAccodion.stories.tsx
+++ src/components/utils/accordion/base-ui/StyledAccodion.stories.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta } from '@storybook/react'
 import { StyledAccordion } from './StyledAccordion'
 import { StyledAccordionSummary } from './StyledAccordionSummary'
 import { StyledAccordionDetails } from './StyledAccordionDetails'
 import { Typography } from '@mui/material'
-import { StyledButton } from 'src/components/inputs/button'
 import { StyledWidgetTitle } from 'src/components/custom/widget/widget-title/StyledWidgetTitle'
 
 const meta: Meta<typeof StyledAccordion> = {
@@ -18,28 +17,30 @@ const meta: Meta<typeof StyledAccordion> = {
 
 export default meta
 
+function AccordionStory() {
+    const [, setIsExpanded] = useState(true)
+
+    return (
+        <div className='max-w-[600px] flex flex-col gap-4'>
+            <StyledAccordion onChange={setIsExpanded}>
+                <div className='flex items-center justify-between pr-4'>
+                    <StyledAccordionSummary className='' size='large' expandChevronRight>
+                        <StyledWidgetTitle>Header lorem ipsum</StyledWidgetTitle>
+                    </StyledAccordionSummary>
+
+                    {/* {isExpanded && <StyledButton size='small' dataTest='test' {...meta.args} variant='contained'>
+                        <div>Button label</div>
+                    </StyledButton>} */}
+                </div>
+
+                <StyledAccordionDetails>
+                    <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
+                </StyledAccordionDetails>
+            </StyledAccordion>
+        </div>
+    )
+}
+
 export const Accordion = {
-    render: () => {
-        const [isExpanded, setIsExpanded] = useState(true)
-
-        return (
-            <div className='max-w-[600px] flex flex-col gap-4'>
-                <StyledAccordion onChange={setIsExpanded}>
-                    <div className='flex items-center justify-between pr-4'>
-                        <StyledAccordionSummary className='' size='large' expandChevronRight>
-                            <StyledWidgetTitle>Header lorem ipsum</StyledWidgetTitle>
-                        </StyledAccordionSummary>
-
-                        {/* {isExpanded && <StyledButton size='small' dataTest='test' {...meta.args} variant='contained'>
-                            <div>Button label</div>
-                        </StyledButton>} */}
-                    </div>
-
-                    <StyledAccordionDetails>
-                        <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Typography>
-                    </StyledAccordionDetails>
-                </StyledAccordion>
-            </div>
-        )
-    },
+    render: () => <AccordionStory />,
 }
