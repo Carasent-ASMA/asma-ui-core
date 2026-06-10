eb9eb311b1af629b98a61b881426f593fb30c12b
maxim-carasent <maxim.curudimov@carasent.com>
2026-06-10 18:00:43 +0300

    ASMA-6520 add gridviewoutlineicon and listviewoutlineicon (#48)
    feat(icons): Add GridViewOutlineIcon and ListViewOutlineIcon

---
+++ src/components/icons/list-view-outline-icon/ListViewOutlineIcon.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from 'react'
+
+export function ListViewOutlineIcon(props: SVGProps<SVGSVGElement>): JSX.Element {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' width={24} height={24} viewBox='0 0 24 24' {...props}>
+            <path
+                fill='currentColor'
+                d='M9.10499 18.373H20.3745V15.2165H9.10499V18.373ZM3.62889 8.78391H7.32236V5.62739H3.62889V8.78391ZM3.62889 13.4589H7.32236V10.5665H3.62889V13.4589ZM3.62889 18.373H7.32236V15.2165H3.62889V18.373ZM9.10499 13.4589H20.3745V10.5665H9.10499V13.4589ZM9.10499 8.78391H20.3745V5.62739H9.10499V8.78391ZM1.64844 20.3535V3.63281H22.3691V20.3535H1.64844Z'
+            ></path>
+        </svg>
+    )
+}
