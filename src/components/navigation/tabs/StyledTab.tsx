450186d90d46387ee7ba3e5e327c16d9d7caaf81
Alexandr Doicov <alexandr.doicov@carasent.com>
2026-06-23 12:04:50 +0300

    feat(navigation): Add textTransform capitalize to StyledTab

--- src/components/navigation/tabs/StyledTab.tsx
+++ src/components/navigation/tabs/StyledTab.tsx
@@ -5,6 +5,7 @@ export const StyledTab: FC<TabProps> = (props) => (
     <Tab
         {...props}
         sx={{
+            textTransform: 'capitalize',
             letterSpacing: 'normal',
             '&.MuiTab-textColorPrimary.Mui-selected': {
                 color: 'var(--colors-gama-500)',
